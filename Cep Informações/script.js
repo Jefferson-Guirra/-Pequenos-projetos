26380751a4f86a8ec9c3b6c708ec06a7f814ec3b
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-08-06 15:37:27 -0300

    Update script.js

--- Cep Informações/script.js
+++ Cep Informações/script.js
@@ -4,31 +4,32 @@ const resultado = document.querySelector('.resultado')
 
 const arrayEvents = ['click']
 
-
-function initCep(){
-  const cep = endereço.cep.value 
+function initCep() {
+  const cep = endereço.cep.value
+  const input = endereço.cep
   resultado.innerText = ''
-  if(cep.length<8){
+  if (cep.length < 8) {
     input.classList.add('erro')
-    input.nextElementSibling.innerText ='cep deve conter 8 digitos'
-  }else{
-    const input = endereço.cep
+    input.nextElementSibling.innerText = 'cep deve conter 8 digitos'
+  }
+  else if (cep.length > 8){
+    input.classList.add('erro')
+    input.nextElementSibling.innerText = 'cep deve conter 8 digitos'
+  } else {
     input.classList.remove('erro')
-    input.nextElementSibling.innerText =''
-    const doc = fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    doc.then(r=>r.json())
-    .then(body=>{
-      const span = document.createElement('span')
-      Object.keys(body).forEach(key=>{
-        resultado.innerHTML += '<span>' +`${key}- ${body[key]}` + '</span>'
+    input.nextElementSibling.innerText = ''
+    console.log(cep)
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(r => r.json())
+      .then(body => {
+        console.log(body)
+        const span = document.createElement('span')
+        Object.keys(body).forEach(key => {
+          resultado.innerHTML += '<span>' + `${key}- ${body[key]}` + '</span>'
+        })
       })
-      
-      
-    })
-
   }
 }
-arrayEvents.forEach(userEvent=>{
-  btn.addEventListener(userEvent,initCep)
+arrayEvents.forEach(userEvent => {
+  btn.addEventListener(userEvent, initCep)
 })
-
